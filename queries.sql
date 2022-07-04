72093eae24923662072fe8c956ab9d11034a78a6
Daniel Malo Osorio <danielmaloosorio@gmail.com>
2022-07-04 17:06:58 -0500

    Add birthdate range of animals query

--- queries.sql
+++ queries.sql
@@ -1,3 +1,7 @@
 /*Queries that provide answers to the questions from all projects.*/
 
 select * from animals where name like '%mon';
+
+select name, date_of_birth from animals where 
+    date_of_birth >= '2016-01-01'
+    and date_of_birth < '2019-12-31';
