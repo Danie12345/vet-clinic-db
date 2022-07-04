2d0d069142129732b1e9d6f8c75e4503c95c0e3a
Daniel Malo Osorio <danielmaloosorio@gmail.com>
2022-07-04 17:09:03 -0500

    Update to only show names

--- queries.sql
+++ queries.sql
@@ -2,6 +2,6 @@
 
 select * from animals where name like '%mon';
 
-select name, date_of_birth from animals where 
+select name from animals where 
     date_of_birth >= '2016-01-01'
     and date_of_birth < '2019-12-31';
