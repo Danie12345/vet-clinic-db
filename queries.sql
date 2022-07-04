941dbf5f1ba6a155738ce31211ec37d8acfd3a17
Daniel Malo Osorio <danielmaloosorio@gmail.com>
2022-07-04 17:11:17 -0500

    Add neutered and few escapes query

--- queries.sql
+++ queries.sql
@@ -5,3 +5,7 @@ select * from animals where name like '%mon';
 select name from animals where 
     date_of_birth >= '2016-01-01'
     and date_of_birth < '2019-12-31';
+
+select name from animals where
+    neutered = true
+    and escape_attempts < 3;
