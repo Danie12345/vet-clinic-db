eef984394b5dd2a7d715eeaa5032cd3a86b48b6c
Daniel Malo Osorio <danielmaloosorio@gmail.com>
2022-07-06 16:08:02 -0500

    Add owners' data

--- data.sql
+++ data.sql
@@ -22,3 +22,15 @@ VALUES
     ('Boarmon', '2005-06-07', 7, true, 20.40),
     ('Blossom', '1998-10-13', 3, true, 17.00),
     ('Ditto', '2022-05-14', 4, true, 22.00);
+
+-- project2
+
+insert into 
+    owners (full_name, age)
+VALUES
+    ('Sam Smith', 34),
+    ('Jennifer Orwell', 19),
+    ('Bob', 45),
+    ('Melody Pond', 77),
+    ('Dean Winchester', 14),
+    ('Jodie Whittaker', 38);
